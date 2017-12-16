8558776cdc05724c2c507272216228d0a823b9c0
Oliver Daniel <oliver.daniel@student.tdsb.on.ca>
2017-12-16 15:46:16 -0500

    Add Riemann-sum plotting feature

--- RiemannGrapher.py
+++ RiemannGrapher.py
@@ -10,8 +10,9 @@ def graph(
         handed: str = 'left',
         lower: float = 0,
         upper: float = 20,
-        pos_color: str = None,
-        neg_color: str = None):
+        pos_color: str = 'blue',
+        neg_color: str = 'red',
+        plot_sum: bool = False):
     '''graphs a function, along with a Riemann sum.'''
 
     diff = upper - lower
@@ -32,6 +33,10 @@ def graph(
     x = np.linspace(lower, upper, n * 20)
     y = f(x)
 
+    riemann_sum = 0
+    sum_x = []
+    sum_y = []
+
     fig, ax = plt.subplots(1)
     for i in np.linspace(lower, upper, num=n):
         val = f(i + offset)
@@ -44,7 +49,12 @@ def graph(
                 color=pos_color if val > 0 else neg_color
             )
         )
+        riemann_sum += width * val
+        sum_x.append(i)
+        sum_y.append(riemann_sum)
 
     plt.plot(x, y)
+    if plot_sum:
+        plt.plot(sum_x, sum_y, 'red')
     plt.xlim(lower, upper)
     plt.show()

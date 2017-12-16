26b22ddd449f769a7d143e282ece9c8c7bd84061
TheVirtualMachine <vincent@vincemacri.ca>
2017-12-16 13:54:29 -0500

    Use text field instead of text line for math entry.

--- GUI.py
+++ GUI.py
@@ -1,5 +1,4 @@
-import tkinter
-from tkinter import ttk
+import tkinter as tk
 
 from sympy import *
 from sympy.abc import x, e
@@ -17,36 +16,36 @@ def renderLatexString(expression):
 
 	string = "\\Huge \\begin{math} \\displaystyle " + expression + " \\end{math}"
 	preview(string, output="png", viewer="file", filename=FORMULA_PNG_FILE, euler=False, preamble=preamble)
-	preview(string, output="pdf", viewer="file", filename=FORMULA_PDF_FILE, euler=False, preamble=preamble)
+	#preview(string, output="pdf", viewer="file", filename=FORMULA_PDF_FILE, euler=False, preamble=preamble)
 
-class Application(ttk.Frame):
+class Application(tk.Frame):
 	def __init__(self, master=None):
 		super().__init__(master)
 		self.pack()
 		self.create_widgets()
 
 	def create_widgets(self):
-		self.formula = ttk.Entry(self)
+		self.formula = tk.Text(self, height=10)
 		self.formula.pack(side="top")
 
-		self.renderButton = ttk.Button(self, text="Render")
+		self.renderButton = tk.Button(self, text="Render")
 		self.renderButton["command"] = self.renderFormula
 		self.renderButton.pack(side="top")
 
 		renderLatexString("e^x")
 		self.formulaImage = ImageTk.PhotoImage(Image.open(FORMULA_PNG_FILE))
-		self.renderedFormula = ttk.Label(self, image=self.formulaImage)
+		self.renderedFormula = tk.Label(self, image=self.formulaImage)
 		self.renderedFormula.pack(side="top")
 
-		self.quitButton = ttk.Button(self, text="Quit", command=root.destroy)
+		self.quitButton = tk.Button(self, text="Quit", command=root.destroy)
 		self.quitButton.pack(side="top")
 
 	def renderFormula(self):
-		renderLatexString(self.formula.get())
+		renderLatexString(self.formula.get("1.0", "end"))
 		self.formulaImage = ImageTk.PhotoImage(Image.open(FORMULA_PNG_FILE))
 		self.renderedFormula.configure(image=self.formulaImage)
 
-root = tkinter.Tk()
+root = tk.Tk()
 root.title("MCV4U0 ISP")
 root.geometry("500x500")
 

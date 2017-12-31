acf08c99f303a79d9e6695f4c887891e683da87d
Oliver Daniel <oliver.daniel@student.tdsb.on.ca>
2017-12-31 14:44:36 -0900

    Finishes and refactors grapher config panel

---
+++ frontend/src/components/home_page/GrapherConfigPanel.js
@@ -0,0 +1,110 @@
+import React, {Component} from 'react';
+import {
+    Row,
+    Col,
+    Input,
+    Button,
+    Icon,
+    Collapsible,
+    CollapsibleItem
+} from 'react-materialize';
+import MathEditor from '../MathEditor';
+import {MaterialPicker} from 'react-color';
+import './HomePage.css';
+
+class GrapherConfigPanel extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            posColor: 'red',
+            negColor: 'blue'
+        }
+    }
+
+    render() {
+        return (
+            <div id="config-panel">
+                <Row id='mathquill' className='aligned'>
+                    <MathEditor
+                        style={{
+                        marginLeft: '10px'
+                    }}>f(x)\ =</MathEditor>
+                    <MathEditor editable/>
+                </Row>
+                <Row id='x-range' className='aligned'>
+                    <Input
+                        validate
+                        type="text"
+                        defaultValue={0}
+                        id='lower-lim'
+                        label='Limits'
+                        icon='space_bar'/>
+                    <MathEditor>\leq\ x\ \leq</MathEditor>
+                    <Input type="text" defaultValue={20} id='upper-lim'/>
+                </Row>
+                <Row
+                    id='n-slider'
+                    className='range-field'
+                    style={{
+                    width: '95%'
+                }}>
+                    <label htmlFor="n-slider">Number of samples</label>
+                    <input type="range" min={5} max={100} step={5} defaultValue={5}/>
+                </Row>
+                <Row
+                    id='handed'
+                    style={{
+                    alignItems: 'center'
+                }}>
+                    <Col s={6} l={4} className='aligned'>
+                        <Input type='select' label='Handedness' defaultValue={'left'} icon='pan_tool'>
+                            <option value='left'>Left</option>
+                            <option value='center'>Center</option>
+                            <option value='right'>Right</option>
+                        </Input>
+                    </Col>
+                    <Col
+                        s={6}
+                        l={4}
+                        style={{
+                        paddingTop: '25px'
+                    }}>
+                        <Input type='checkbox' label='Graph Running Area'/>
+                    </Col>
+                    <Col s={12} l={4}>
+                        <label>Colours</label>
+                        <Collapsible
+                            style={{
+                            'i': {
+                                color: 'red'
+                            }
+                        }}>
+                            <CollapsibleItem
+                                header='Positive'
+                                icon='add_circle'
+                                style={{
+                                color: this.state.posColor
+                            }}>
+                                <MaterialPicker/>
+                            </CollapsibleItem>
+                            <CollapsibleItem
+                                header='Negative'
+                                icon='remove_circle'
+                                style={{
+                                color: this.state.negColor
+                            }}>
+                                <MaterialPicker/>
+                            </CollapsibleItem>
+                        </Collapsible>
+                    </Col>
+                </Row>
+                <Row id='graph-btn' className='aligned centered'>
+                    <Button className='red'>Graph
+                        <Icon left>show_chart</Icon>
+                    </Button>
+                </Row>
+            </div>
+        )
+    }
+}
+export default GrapherConfigPanel;
--- frontend/src/components/home_page/HomePage.js
+++ frontend/src/components/home_page/HomePage.js
@@ -1,68 +1,17 @@
 import React, {Component} from 'react';
-import {Navbar, Row, Col, Input, Icon} from 'react-materialize';
+import {Navbar, Row, Col} from 'react-materialize';
+import GrapherConfigPanel from './GrapherConfigPanel';
 import './HomePage.css';
 
-const {MathQuill} = window;
-const MQ = MathQuill.getInterface(2);
-
 class HomePage extends Component {
-
-  componentDidMount() {
-    for (let span of document.getElementsByClassName('static-math')) {
-      MQ.StaticMath(span);
-    }
-
-    let mathFieldSpan = document.getElementById('math-field');
-    var mathField = MQ.MathField(mathFieldSpan, {
-      spaceBehavesLikeTab: true,
-      autoCommands: 'pi sqrt',
-      handlers: {
-        edit: function () {
-          console.log(mathField.latex());
-        }
-      }
-    });
-  }
 
   render() {
     return (
       <div className="HomePage">
         <Navbar brand='Riemann Sum Calculator'></Navbar>
-
         <Row id="calculators">
           <Col s={12} m={6}>
-            <Row id='mathquill'>
-              <span className="homepage__math static-math">f(x)\ =</span>
-              <span className="homepage__math" id="math-field"></span>
-            </Row>
-            <Row id='x-range' className="centered-aligned">
-              <Input
-                validate
-                type="text"
-                defaultValue={0}
-                id='lower-lim'
-                label='Limits'
-                icon='space_bar'/>
-              <span className="homepage__math static-math">\leq\ x\ \leq</span>
-              <Input validate type="text" defaultValue={20} id='upper-lim'/>
-            </Row>
-            <Row id='n-slider'></Row>
-            <Row id='handed'>
-              <Col s={12} m={6}>
-                <Input type='select' label='Handedness' defaultValue={'left'} icon='pan_tool'>
-                  <option value='left'>Left</option>
-                  <option value='center'>Center</option>
-                  <option value='right'>Right</option>
-                </Input>
-              </Col>
-              <Col s={12} m={6}>
-                <Icon>show_chart</Icon>
-                <Input type='checkbox' label='Graph Running Area'/>
-              </Col>
-            </Row>
-            <Row id='mathquill'></Row>
-            <Row id='running-area'></Row>
-            <Row id='graph-btn'></Row>
+            <GrapherConfigPanel/>
           </Col>
           <Col s={12} m={6}>
             B
@@ -73,5 +22,4 @@ class HomePage extends Component {
     );
   }
 }
-
 export default HomePage;
